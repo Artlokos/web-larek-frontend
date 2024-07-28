c4bc6224c7999b1f0601f5b964076c1dfcadbd0e
ArtLokos <artlokos@yandex.ru>
2024-07-28 18:21:57 +1000

    28072024

--- src/components/model/AppModel.ts
+++ src/components/model/AppModel.ts
@@ -1,37 +1,120 @@
-// import { ProductItem } from "./ProductItemsData";
-// import { IOrderData } from "../../types";
-// export interface IAppState {
-//     // Корзина с товарами
-//     basket: ProductItem[];
-//     // Массив карточек товара
-//     productItemList: ProductItem[];
-//     // Информация о заказе при покупке товара
-//     order: IOrderData;
-//     // Ошибки при заполнении форм
-//     formErrors: FormErrors;
-//     // Метод для добавления товара в корзину
-//     addToBasket(value: ProductItem): void;
-//     // Метод для удаления товара из корзины
-//     deleteFromBasket(id: string): void;
-//     // Метод для полной очистки корзины
-//     clearBasket(): void;
-//     // Метод для получения количества товаров в корзине
-//     getBasketAmount(): number;
-//     // Метод для получения суммы цены всех товаров в корзине
-//     getTotalBasketPrice(): number;
-//     // Метод для добавления ID товаров в корзине в поле items для order
-//     setItems(): void;
-//     // Метод для заполнения полей email, phone, address, payment в order
-//     setOrderField(field: keyof IOrderForm, value: string): void;
-//     // Валидация форм для окошка "контакты"
-//     validateContacts(): boolean;
-//     // Валидация форм для окошка "заказ"
-//     validateOrder(): boolean;
-//     // Очистить order после покупки товаров
-//     refreshOrder(): boolean;
-//     // Метод для превращения данных, полученых с сервера в тип данных приложения
-//     setproductItemList(items: IProduct[]): void;
-//     // Метод для обновления поля selected во всех товарах после совершения покупки
-//     resetSelected(): void;
-//   }
-  
+
+import { productItemList } from './ProductItemsData';
+import { IOrder, IProductItem, FormErrors, IOrderForm } from '../types';
+import { Model } from './base/Model';
+import { IAppState } from '../types';
+
+export class Product {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+  selected: boolean;
+}
+
+/*
+  * Класс, описывающий состояние приложения
+  * */
+export class AppModel{
+  // Корзина с товарами
+  basketItem: ProductItem[] = [];
+
+  // Массив со всеми товарами
+  productItemList: Product[];
+
+  // Объект заказа клиента
+  order: IOrder = {
+    items: [],
+    payment: '',
+    total: null,
+    address: '',
+    email: '',
+    phone: '',
+  };
+
+  // Объект с ошибками форм
+  formErrors: FormErrors = {};
+
+  addToBasket(value: Product) {
+    this.basket.push(value);
+  }
+
+  deleteFromBasket(id: string) {
+    this.basket = this.basket.filter(item => item.id !== id)
+  }
+
+  clearBasket() {
+    this.basket.length = 0;
+  }
+
+  getBasketAmount() {
+    return this.basket.length;
+  }
+
+  setItems() {
+    this.order.items = this.basket.map(item => item.id)
+  }
+
+  setOrderField(field: keyof IOrderForm, value: string) {
+    this.order[field] = value;
+
+    if (this.validateContacts()) {
+      this.events.emit('contacts:ready', this.order)
+    }
+    if (this.validateOrder()) {
+      this.events.emit('order:ready', this.order);
+    }
+  }
+
+  validateContacts() {
+    const errors: typeof this.formErrors = {};
+    if (!this.order.email) {
+      errors.email = 'Необходимо указать email';
+    }
+    if (!this.order.phone) {
+      errors.phone = 'Необходимо указать телефон';
+    }
+    this.formErrors = errors;
+    this.events.emit('contactsFormErrors:change', this.formErrors);
+    return Object.keys(errors).length === 0;
+  }
+
+  validateOrder() {
+    const errors: typeof this.formErrors = {};
+    if (!this.order.address) {
+      errors.address = 'Необходимо указать адрес';
+    }
+    if (!this.order.payment) {
+      errors.payment = 'Необходимо указать способ оплаты';
+    }
+    this.formErrors = errors;
+    this.events.emit('orderFormErrors:change', this.formErrors);
+    return Object.keys(errors).length === 0;
+  }
+
+  refreshOrder() {
+    this.order = {
+      items: [],
+      total: null,
+      address: '',
+      email: '',
+      phone: '',
+      payment: ''
+    };
+  }
+
+  getTotalBasketPrice() {
+    return this.basket.reduce((sum, next) => sum + next.price, 0);
+  }
+
+  setStore(items: IProduct[]) {
+    this.store = items.map((item) => new Product({ ...item, selected: false }, this.events));
+    this.emitChanges('items:changed', { store: this.store });
+  }
+
+  resetSelected() {
+    this.store.forEach(item => item.selected = false)
+  }
+}
